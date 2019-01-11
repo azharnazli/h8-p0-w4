4d7f44db67b0741471df4cce0fbd231d8059bab6
Azhar Nazli <mazhar.nazli@gmail.com>
2019-01-11 16:21:34 +0700

    perbaikan exercise 4

--- exercise4.js
+++ exercise4.js
@@ -1,18 +1,22 @@
 function cariModus(arr) {
+    // you can only write your code here!
     var hasil = 0
+    var maxCount = 0
+
     for (var i = 0; i < arr.length; i++) {
-        var tmp = 0
+        var count = 0
         for (var j = 0; j < arr.length; j++) {
             if (arr[i] === arr[j] && i !== j) {
-                hasil = arr[i];
-                tmp++
-                if (tmp >= 2) {
-                    hasil = -1
+                count++
+                if (count > maxCount) {
+                    maxCount = count
+                    hasil = arr[i]
                 }
             }
         }
+
     }
-    if (hasil === 0) {
+    if (maxCount === 0 || maxCount === arr.length - 1) {
         return -1
     } else {
         return hasil
@@ -21,7 +25,7 @@ function cariModus(arr) {
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
-console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([5, 10, 10, 10, 6, 5])); // 5
 console.log(cariModus([10, 3, 1, 2, 5])); // -1
 console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
 console.log(cariModus([7, 7, 7, 7, 7])); // -1
