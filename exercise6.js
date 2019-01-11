9fc8eb5773ecba9f517c458364c5485cf04d73a3
Azhar Nazli <mazhar.nazli@gmail.com>
2019-01-11 16:31:49 +0700

    perbaikan exercise 6

---
+++ exercise6.js
@@ -0,0 +1,27 @@
+function digitPerkalianMinimum(angka) {
+    // you can only write your code here!
+    var result = ""
+    var array = []
+    for (var i = 0; i <= angka; i++) {
+        for (var j = 0; j <= angka; j++) {
+            if (i * j === angka) {
+                result = i + "" + j
+                array.push(result)
+            }
+        }
+    }
+    var maxNumber = angka * 5
+    for (var k = 0; k < array.length; k++) {
+        if (array[k].length < maxNumber) {
+            maxNumber = array[k].length
+        }
+    }
+    return maxNumber
+}
+
+// TEST CASES
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(20)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
