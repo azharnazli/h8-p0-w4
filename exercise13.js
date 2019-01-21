5f8555aa7cebefd0850efef52a85086fd2f8a86c
azharnazli <mazhar.nazli@gmail.com>
2019-01-21 10:23:15 +0700

    perbaikan exercise13

--- exercise13.js
+++ exercise13.js
@@ -1,27 +1,26 @@
-function deepSum(arr) {
-  var hasil = 0;
+function deepSum (arr) {
+  if (arr.length === 0) {
+    return 'No Number'
+  }
+  var hasil = 0
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr[i].length; j++) {
       for (var k = 0; k < arr[i][j].length; k++) {
-        hasil += arr[i][j][k];
+        hasil += arr[i][j][k]
       }
     }
   }
-  if (arr.length === 0) {
-    return "No Number";
-  } else {
-    return hasil;
-  }
+  return hasil
 }
 
-//TEST CASE
+// TEST CASE
 console.log(
   deepSum([
     [[4, 5, 6], [9, 1, 2, 10], [9, 4, 3]],
     [[4, 14, 31], [9, 10, 18, 12, 20], [1, 4, 90]],
     [[2, 5, 10], [3, 4, 5], [2, 4, 5, 10]]
   ])
-); // 316
+) // 316
 
 console.log(
   deepSum([
@@ -30,6 +29,6 @@ console.log(
     [[3, 5, 1], [1, 5, 3], [1]],
     [[2]]
   ])
-); // 156
+) // 156
 
-console.log(deepSum([])); // No number
+console.log(deepSum([])) // No number
